54c6cf64c97d265e04102412b966cebf035d8097
cecoeqk <38364756+cecoeqk@users.noreply.github.com>
2018-04-14 00:54:19 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+#kottrqbot by ceco
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import bot
+import asyncio
+
+bot - commands.bot(command_prefix='/')
+
+@bot.event
+async del no_ready():
+    print ("az sum qk xD")
+    print ("I am running on" + bot.user.name)
+    print "With th ID:" + (bot.user.id)
+
+bot.run("NDM0NDQ3NjI1NTY4Mzg3MDcy.DbKy-A.2QlmiS1VpfSIWpgoXzzaG-N9AT0")
